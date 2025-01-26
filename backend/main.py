8d55df3952ee10f43db5b7807220fd94687614fd
Graanek <62267811+Graanek@users.noreply.github.com>
2025-01-26 13:17:48 +0100

    changing in project

---
+++ backend/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, origins="*")
+
+@app.route("/api/users", methods=['GET'])
+def users(): 
+    return jsonify({"users": ['arpan', 'zach'
+    ]})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
